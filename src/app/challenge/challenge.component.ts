b74d51c55e3d867480528d1c539ed4944a1fb781
Euan Goddard <euan.goddard@gmail.com>
2016-11-23 21:56:04 +0000

    Added inter-challenge navigation

--- src/app/challenge/challenge.component.ts
+++ src/app/challenge/challenge.component.ts
@@ -1,3 +1,4 @@
+import { TunesService } from './tunes.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Tune } from './models';
@@ -11,11 +12,40 @@ export class ChallengeComponent implements OnInit {
 
   public tune: Tune;
 
-  constructor(private route: ActivatedRoute) {
+  public previousTuneIndex: number;
+
+  public nextTuneIndex: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private tunes: TunesService,
+  ) {
 
   }
 
   ngOnInit() {
-    this.tune = this.route.snapshot.data['tune'];
+    this.route.data.subscribe(data => {
+      this.tune = data['tune'];
+    });
+
+    this.route.params
+      .map(params => +params['id'])
+      .subscribe(tuneIndex => this.setupNavigation(tuneIndex));
+  }
+
+  private setupNavigation(currentTuneIndex: number): void {
+    const previousTuneIndex = currentTuneIndex - 1;
+    if (this.tunes.getTuneForIndex(previousTuneIndex)) {
+      this.previousTuneIndex = previousTuneIndex;
+    } else {
+      this.previousTuneIndex = null;
+    }
+
+    const nextTuneIndex = currentTuneIndex + 1;
+    if (this.tunes.getTuneForIndex(nextTuneIndex)) {
+      this.nextTuneIndex = nextTuneIndex;
+    } else {
+      this.nextTuneIndex = null;
+    }
   }
 }
