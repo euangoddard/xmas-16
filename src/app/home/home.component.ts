5d93c0bbac4af6d6bdeb3412b82032efc88eee1f
Euan Goddard <euan.goddard@gmail.com>
2016-12-03 08:48:10 +0000

    Added content to the start page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,14 @@
+import { TUNES } from './../challenge/tunes.definition';
+import { Tune } from './../challenge/models';
 import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './home.component.html',
   selector: 'app-home',
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  get tunes(): Tune[] {
+    return TUNES;
+  }
+}
